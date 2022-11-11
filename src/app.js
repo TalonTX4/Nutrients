5d5ccf70f50544ebf5cf73f3fffdfc5a158cf232
BuildTools <unconfigured@null.spigotmc.org>
2022-11-11 15:33:35 -0600

    Initial commit from tutorial

---
+++ src/app.js
@@ -0,0 +1,76 @@
+// imports
+const express = require('express');
+const mysql = require("mysql")
+const bcrypt = require("bcryptjs")
+
+const app = express();
+
+
+// other imports
+const path = require("path")
+const {query} = require("express");
+
+const publicDir = path.join(__dirname, './public')
+
+app.use(express.urlencoded({extended: 'false'}))
+app.use(express.json())
+app.use(express.static(publicDir))
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "Joseph",
+    password: "987521741859875275391",
+    database: "Nutrients"
+})
+
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MySQL connected!")
+    }
+})
+
+app.set('view engine', 'hbs')
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.post("/auth/register", (req, res) => {
+    const { name, email, password, password_confirm } = req.body
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, result) => {
+        if(error){
+            console.log(error)
+        }
+        // fixme no check to prevent email reuse
+
+        // fixme no password confirmation
+        let hashedPassword = await bcrypt.hash(password, 8)
+
+        console.log(hashedPassword)
+
+        db.query('INSERT INTO users SET?', {name: name, email: email, password: hashedPassword}, (err, result) => {
+            if(error) {
+                console.log(error)
+            } else {
+                return res.render('register', {
+                    message: 'User registered!'
+                })
+            }
+        })
+    })
+})
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.listen(5000, ()=> {
+    console.log("server started on port 5000")
+});
